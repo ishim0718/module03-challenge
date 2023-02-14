60338caaa593ee03cc328d16a5a3ef8ed1d0c11a
ishim0718 <ishim0718@gmail.com>
2023-02-13 21:09:22 -0500

    added func for random generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,36 @@
 // Assignment code here
+//funtions to create random values
+function randomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function randomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function randomUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function randomSymbol() {
+  const specialChar = '!"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'; 
+  return specialChar[Math.floor(Math.random() * specialChar.legnth)];
+} 
+
+//set password length
+var passwordLength = prompt("How long would you like the password to be?", "Input a number between 8 and 128")
+
+if (passwordLength != null) {
+  prompt("Please enter a valid number between 8 and 128")
+}
+else (passwordLength < 8) {
+  prompt("Please enter a valid number between 8 and 128")
+}
+else (passwordLength > 128) {
+  prompt("Please enter a valid number between 8 and 128")
+}
+else  (Number.isInteger(passwordLength) == false) {
+  prompt("Please enter a valid number between 8 and 128")
+}
+ return;
+
 
 
 // Get references to the #generate element
